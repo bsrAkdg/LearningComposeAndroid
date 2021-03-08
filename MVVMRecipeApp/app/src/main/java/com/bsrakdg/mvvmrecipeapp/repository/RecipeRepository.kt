d780b04f5e4e19c29a399609459625fa9b17b0a1
bserdaroglu <busraaakdag@gmail.com>
2021-03-08 22:55:35 +0300

    Repository setup

---
+++ MVVMRecipeApp/app/src/main/java/com/bsrakdg/mvvmrecipeapp/repository/RecipeRepository.kt
@@ -0,0 +1,9 @@
+package com.bsrakdg.mvvmrecipeapp.repository
+
+import com.bsrakdg.mvvmrecipeapp.domain.model.Recipe
+
+interface RecipeRepository {
+
+    suspend fun search(token: String, page: Int, query: String): List<Recipe>
+    suspend fun get(token: String, id: Int) : Recipe
+}
---
+++ MVVMRecipeApp/app/src/main/java/com/bsrakdg/mvvmrecipeapp/repository/RecipeRepositoryImpl.kt
@@ -0,0 +1,30 @@
+package com.bsrakdg.mvvmrecipeapp.repository
+
+import com.bsrakdg.mvvmrecipeapp.domain.model.Recipe
+import com.bsrakdg.mvvmrecipeapp.network.RecipeService
+import com.bsrakdg.mvvmrecipeapp.network.model.RecipeDtoMapper
+
+class RecipeRepositoryImpl(
+    private val recipeService: RecipeService,
+    private val mapper: RecipeDtoMapper
+) : RecipeRepository {
+
+    override suspend fun search(token: String, page: Int, query: String): List<Recipe> {
+        return mapper.toDomainList(
+            recipeService.search(
+                token = token,
+                page = page,
+                query = query
+            ).recipes
+        )
+    }
+
+    override suspend fun get(token: String, id: Int): Recipe {
+        return mapper.mapToDomainModel(
+            recipeService.get(
+                token = token,
+                id = id
+            )
+        )
+    }
+}
